4c556876542f4bbf8ebaae4cd48175bb41561443
r3daaa <r3da@protonmail.com>
2019-11-02 01:09:06 +0100

    day06

--- ex18.py
+++ ex18.py
@@ -1,2 +1,18 @@
-def print_two(*argv):
-    
+#def print_two(*args):
+#    arg1, arg2 = args
+#    print(f"arg1: {arg1}, arg2: {arg2}")
+
+#no need for *args
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+#one arg
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("i got nothin'.")
+
+#print_two("reda","rakman")
+print_two_again("reda","rakman")
+print_one("hamouda")
+print_none()
