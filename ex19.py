3b6211ff18c205b828855970ae5c9c7ce7b7a3e5
r3daaa <r3da@protonmail.com>
2019-11-02 16:09:01 +0100

    day ex19 + study drill ;)

--- ex19.py
+++ ex19.py
@@ -21,3 +21,12 @@ print("and we can combine the two variables and math :")
 cheese_and_crackers(amount_of_cheese + 100,amount_of_crackers+1000)
 
 #so in this exo i learnt the syntax of function and how to integrate variables and maths inside.
+#this is study drill , i m creating a funtion that takes emails from input then add them to a text file
+def collect_emails_in_text_file(email):
+    print(f"{email}")
+    out_file = open("email.txt",'a')
+    out_file.write(email + '\n')
+    out_file.close()
+
+email=input("tape your email here :")
+collect_emails_in_text_file(email)
